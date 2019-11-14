67fe4b3a8d6fc4b5c76e2199d9a6cd855b87142e
qiaoyongtian <qiaoyongtian@yunfutech.com>
2019-11-14 08:52:28 +0800

    1114

---
+++ test_fastnlp/test_log.py
@@ -0,0 +1,12 @@
+"""测试log模块"""
+from fastNLP import logger
+
+
+if __name__ == "__main__":
+    logger.info('测试一下info')
+    logger.debug('测试一下debug')
+    logger.warning('测试一下warning')
+    logger.error('测试一下error')
+    foo_bar(width, height, color='black', design=None, x='foo',
+            emphasis=None, highlight=0)
+
